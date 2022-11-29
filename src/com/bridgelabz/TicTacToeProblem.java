21a4c59ae4657db36ecb410989f8c32aa826a0ca
Kavyasree-J-Ganesh <kavyasreejganesh97@gmail.com>
2022-11-29 19:27:54 +0530

    [Kavyasree J Ganesh] UC2_TicTacToe Problem

--- src/com/bridgelabz/TicTacToeProblem.java
+++ src/com/bridgelabz/TicTacToeProblem.java
@@ -2,4 +2,17 @@ package com.bridgelabz;
 
 public class TicTacToeProblem {
     static char[] board = new char[0];
+
+    char input() {
+        int randomChar = (int) Math.floor(Math.random() * 10 % 2);
+        if (randomChar == 1) {
+            return 'x';
+        } else {
+            return '0';
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
