d20f6710e5cccabe9868fbc4320968b158269ff6
Kavyasree-J-Ganesh <kavyasreejganesh97@gmail.com>
2022-11-29 20:54:14 +0530

    [Kavyasree J Ganesh] UC7_TicTacToe Problem

--- src/com/bridgelabz/TicTacToeProblem.java
+++ src/com/bridgelabz/TicTacToeProblem.java
@@ -13,8 +13,46 @@ public class TicTacToeProblem {
         }
     }
 
-    static String checkWinner()
-    {
+    static String checkWinner(){
+        for (int a = 0; a < 8; a++) {
+            String line = null;
+
+            switch (a) {
+                case 0:
+                    line = board[0] + board[1] + board[2];
+                    break;
+                case 1:
+                    line = board[3] + board[4] + board[5];
+                    break;
+                case 2:
+                    line = board[6] + board[7] + board[8];
+                    break;
+                case 3:
+                    line = board[0] + board[3] + board[6];
+                    break;
+                case 4:
+                    line = board[1] + board[4] + board[7];
+                    break;
+                case 5:
+                    line = board[2] + board[5] + board[8];
+                    break;
+                case 6:
+                    line = board[0] + board[4] + board[8];
+                    break;
+                case 7:
+                    line = board[2] + board[4] + board[6];
+                    break;
+            }
+            //For X winner
+            if (line.equals("XXX")) {
+                return "X";
+            }
+
+            // For O winner
+            else if (line.equals("OOO")) {
+                return "O";
+            }
+        }
 
         for (int a = 0; a < 9; a++) {
             if (Arrays.asList(board).contains(
@@ -39,12 +77,13 @@ public class TicTacToeProblem {
         turn = "X";
         System.out.println(
                 "X will play first. Enter a slot number to place X in:");
+        for (int a = 0; a < 9; a++) {
+            board[a] = String.valueOf(a + 1);
+        }
 
         while (winner == null) {
             int numInput = in.nextInt();
-            for (int a = 0; a < 9; a++) {
-                board[a] = String.valueOf(a + 1);
-            }
+
             if (board[numInput - 1].equals(
                     String.valueOf(numInput))) {
                 board[numInput - 1] = turn;
