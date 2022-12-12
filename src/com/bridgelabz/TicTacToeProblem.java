bcea7d26951fff19a6f4eb7b89aab9f162db501e
Kavyasree-J-Ganesh <kavyasreejganesh97@gmail.com>
2022-12-13 03:50:50 +0530

    [Kavyasree J Ganesh] Tic tac toe problrm

--- src/com/bridgelabz/TicTacToeProblem.java
+++ src/com/bridgelabz/TicTacToeProblem.java
@@ -205,7 +205,13 @@ public class TicTacToeProblem {
                 System.out.println("Sorry! Computer has won! Thanks for playing.");
             }
         }
-        
+        System.out.println("Type 1 to continue and 0 to stop");
+        int replay = in.nextInt();
+        System.out.println("hi");
+        if(replay == 1) {
+
+            startGame();
+        }
     }
     public static void main(String[] args) {
         startGame();
